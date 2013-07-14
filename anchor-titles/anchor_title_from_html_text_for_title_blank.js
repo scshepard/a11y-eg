dc734e2eab948eb8e2594601e3e2122fa0b712d0
Sumit. M. Asok <sumitasok@gmail.com>
2013-07-15 02:34:12 +0530

    anchor_title_from_html_text_for_title_blank js

---
+++ anchor-titles/anchor_title_from_html_text_for_title_blank.js
@@ -0,0 +1,9 @@
+jQuery.each(jQuery('a'), function(index, value) {
+    if (jQuery(value).attr('title') == '') {
+        jQuery(value).attr('title', $(value).text());
+    }
+});
+    
+    // escape &amp; html elements
+    // escape <img> tags
+    // escape & in link href
